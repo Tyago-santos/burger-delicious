40f551c9c1667b83a333e6bcae0baf4ebdb96efc
tiago santos <xlzthyago@gmail.com>
2026-01-22 20:44:56 -0300

    "fix: fix header menu"

--- src/main.ts
+++ src/main.ts
@@ -1,35 +1,35 @@
 class Menu {
-    private btnEl;
-    private menuEl;
-    private btnTarget;
+  private btnEl;
+  private menuEl;
+  private btnTarget;
 
-    constructor(btnClass: string, menuClass: string) {
-        this.btnEl = document.querySelector(btnClass) as HTMLElement;
-        this.menuEl = document.querySelector(menuClass) as HTMLUListElement;
+  constructor(btnClass: string, menuClass: string) {
+    this.btnEl = document.querySelector(btnClass) as HTMLElement;
+    this.menuEl = document.querySelector(menuClass) as HTMLUListElement;
 
-        this.initialEvents();
+    this.initialEvents();
 
-        this.btnTarget = 'menuMobile' as string;
+    this.btnTarget = 'menuMobile' as string;
+  }
+
+  toggleMenu = (ev: Event) => {
+    const elTarget = ev.target as HTMLElement;
+    if (elTarget.id == this.btnTarget) {
+      if (this.menuEl.classList.contains('-translate-x-full')) {
+        this.menuEl.classList.remove('-translate-x-full');
+        this.menuEl.classList.add('translate-x-0');
+      } else {
+        this.menuEl.classList.add('-translate-x-full');
+        this.menuEl.classList.remove('translate-x-0');
+      }
     }
+  };
 
-    toggleMenu = (ev: Event) => {
-        const elTarget = ev.target as HTMLElement;
-        if (elTarget.id == this.btnTarget) {
-            if (this.menuEl.classList.contains('-translate-x-full')) {
-                this.menuEl.classList.remove('-translate-x-full');
-                this.menuEl.classList.add('translate-x-0');
-            } else {
-                this.menuEl.classList.add('-translate-x-full');
-                this.menuEl.classList.remove('translate-x-0');
-            }
-        }
-    };
-
-    initialEvents = () => {
-        this.btnEl.addEventListener('click', this.toggleMenu);
-    };
+  initialEvents = () => {
+    this.btnEl.addEventListener('click', this.toggleMenu);
+  };
 }
 
 window.onload = () => {
-    new Menu('nav', 'ul');
+  new Menu('nav', '#toggleMenu');
 };
